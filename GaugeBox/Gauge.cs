56b206a34b95d1cf3cc61bf382f25f942876d47e
Johan Larsson <johan_gavle1@hotmail.com>
2014-05-17 18:26:31 +0200

    Not moving yet

--- GaugeBox/Gauge.cs
+++ GaugeBox/Gauge.cs
@@ -1,15 +1,19 @@
 ﻿namespace GaugeBox
 {
+    using System;
     using System.Windows;
     using System.Windows.Controls.Primitives;
     using System.Windows.Media;
 
     [TemplatePart(Name = IndicatorTemplateName, Type = typeof(FrameworkElement))]
+    [TemplatePart(Name = TrackTemplateName, Type = typeof(FrameworkElement))]
     public class Gauge : RangeBase
     {
         private FrameworkElement _indicator;
-        private TranslateTransform _indicatorTransform;
+        private FrameworkElement _track;
+        private readonly TranslateTransform _indicatorTransform = new TranslateTransform();
         private const string IndicatorTemplateName = "PART_Indicator";
+        private const string TrackTemplateName = "PART_Track";
 
         public Gauge()
         {
@@ -21,12 +25,47 @@
         /// </summary>
         public override void OnApplyTemplate()
         {
+            if (Template == null)
+            {
+                return;
+            }
             base.OnApplyTemplate();
             this._indicator = this.GetTemplateChild(IndicatorTemplateName) as FrameworkElement;
+            this._track = this.GetTemplateChild(TrackTemplateName) as FrameworkElement;
             if (_indicator != null)
             {
-                //if(_indicator.RenderTransform.a)
+                _indicator.RenderTransform = _indicatorTransform;
             }
+        }
+
+        /// <summary>
+        /// Updates the current position of the <see cref="T:System.Windows.Controls.ProgressBar"/> when the <see cref="P:System.Windows.Controls.Primitives.RangeBase.Minimum"/> property changes.
+        /// </summary>
+        /// <param name="oldMinimum">Old value of the <see cref="P:System.Windows.Controls.Primitives.RangeBase.Minimum"/> property.</param><param name="newMinimum">New value of the <see cref="P:System.Windows.Controls.Primitives.RangeBase.Minimum"/> property.</param>
+        protected override void OnMinimumChanged(double oldMinimum, double newMinimum)
+        {
+            base.OnMinimumChanged(oldMinimum, newMinimum);
+            this.SetIndicatorPos();
+        }
+
+        /// <summary>
+        /// Updates the current position of the <see cref="T:System.Windows.Controls.ProgressBar"/> when the <see cref="P:System.Windows.Controls.Primitives.RangeBase.Maximum"/> property changes.
+        /// </summary>
+        /// <param name="oldMaximum">Old value of the <see cref="P:System.Windows.Controls.Primitives.RangeBase.Maximum"/> property.</param><param name="newMaximum">New value of the <see cref="P:System.Windows.Controls.Primitives.RangeBase.Maximum"/> property.</param>
+        protected override void OnMaximumChanged(double oldMaximum, double newMaximum)
+        {
+            base.OnMaximumChanged(oldMaximum, newMaximum);
+            this.SetIndicatorPos();
+        }
+
+        /// <summary>
+        /// Updates the current position of the <see cref="T:System.Windows.Controls.ProgressBar"/> when the <see cref="P:System.Windows.Controls.Primitives.RangeBase.Value"/> property changes.
+        /// </summary>
+        /// <param name="oldValue">Old value of the <see cref="P:System.Windows.Controls.Primitives.RangeBase.Value"/> property.</param><param name="newValue">New value of the <see cref="P:System.Windows.Controls.Primitives.RangeBase.Value"/> property.</param>
+        protected override void OnValueChanged(double oldValue, double newValue)
+        {
+            base.OnValueChanged(oldValue, newValue);
+            this.SetIndicatorPos();
         }
 
         private void SetIndicatorPos()
@@ -36,7 +75,7 @@
             double minimum = this.Minimum;
             double maximum = this.Maximum;
             double num = this.Value;
-            //this._indicator.RenderTransform =  (num - minimum) / (maximum - minimum);
+            _indicatorTransform.X = Math.Abs(num - minimum)/Math.Abs(maximum - minimum);
         }
     }
 }
