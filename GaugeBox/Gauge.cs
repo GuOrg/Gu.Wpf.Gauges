57d5af61737800f3e7f24830e734007f191fb59d
Johan Larsson <johan_gavle1@hotmail.com>
2014-05-17 17:49:32 +0200

    Initial

---
+++ GaugeBox/Gauge.cs
@@ -0,0 +1,42 @@
+﻿namespace GaugeBox
+{
+    using System.Windows;
+    using System.Windows.Controls.Primitives;
+    using System.Windows.Media;
+
+    [TemplatePart(Name = IndicatorTemplateName, Type = typeof(FrameworkElement))]
+    public class Gauge : RangeBase
+    {
+        private FrameworkElement _indicator;
+        private TranslateTransform _indicatorTransform;
+        private const string IndicatorTemplateName = "PART_Indicator";
+
+        public Gauge()
+        {
+
+        }
+
+        /// <summary>
+        /// Called when a template is applied to a <see cref="T:System.Windows.Controls.ProgressBar"/>.
+        /// </summary>
+        public override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            this._indicator = this.GetTemplateChild(IndicatorTemplateName) as FrameworkElement;
+            if (_indicator != null)
+            {
+                //if(_indicator.RenderTransform.a)
+            }
+        }
+
+        private void SetIndicatorPos()
+        {
+            if (this._indicator == null)
+                return;
+            double minimum = this.Minimum;
+            double maximum = this.Maximum;
+            double num = this.Value;
+            //this._indicator.RenderTransform =  (num - minimum) / (maximum - minimum);
+        }
+    }
+}
---
+++ GaugeBox/Vm.cs
@@ -0,0 +1,62 @@
+﻿namespace GaugeBox
+{
+    using System.ComponentModel;
+    using System.Runtime.CompilerServices;
+    using Annotations;
+
+    public class Vm : INotifyPropertyChanged
+    {
+        private double _value;
+        private double _min;
+        private double _max;
+
+        public Vm()
+        {
+            Min = -1.5;
+            Max = 1.5;
+            Value = 0.3;
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public double Value
+        {
+            get { return _value; }
+            set
+            {
+                if (value.Equals(_value)) return;
+                _value = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Min
+        {
+            get { return _min; }
+            set
+            {
+                if (value.Equals(_min)) return;
+                _min = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public double Max
+        {
+            get { return _max; }
+            set
+            {
+                if (value.Equals(_max)) return;
+                _max = value;
+                OnPropertyChanged();
+            }
+        }
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null) handler(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
