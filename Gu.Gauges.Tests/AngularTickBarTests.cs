cbdcaf7ceb8e3bf2d81d2ed7bc03c638e07feded
Johan Larsson <johan_gavle1@hotmail.com>
2015-02-20 02:31:25 +0100

    AngularTickBar

---
+++ Gu.Gauges.Tests/AngularTickBarTests.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Linq.Expressions;
+using System.Windows.Controls.Primitives;
+
+namespace Gu.Gauges.Tests
+{
+    using NUnit.Framework;
+
+    [RequiresSTA]
+    public class AngularTickBarTests
+    {
+
+    }
+}
