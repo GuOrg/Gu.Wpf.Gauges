783d60583fdf638223eee1aa8935c42664ce1900
Johan Larsson <johan_gavle1@hotmail.com>
2015-02-27 20:01:26 +0100

    WIP LinearAxis

---
+++ Gu.Gauges/LinearAxis.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Gauges
+{
+    using System.Windows;
+
+    public class LinearAxis : Axis
+    {
+        static LinearAxis()
+        {
+            DefaultStyleKeyProperty.OverrideMetadata(typeof(LinearAxis), new FrameworkPropertyMetadata(typeof(LinearAxis)));
+        }
+    }
+}
