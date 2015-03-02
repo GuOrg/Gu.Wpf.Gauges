63be80fd9381778c80646c6c48343bfe455553ee
Johan Larsson <johan_gavle1@hotmail.com>
2015-03-02 20:45:17 +0100

    Finished default linear style now I think.

---
+++ Gu.Gauges/Helpers/NameOf.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+
+namespace Gu.Gauges
+{
+    public static class NameOf
+    {
+        public static string Property(Expression<Func<object>> property)
+        {
+            var pathVisitor = new PathVisitor();
+            pathVisitor.Visit(property);
+            return string.Join(".", pathVisitor.Properties.Select(x => x.Name));
+        }
+
+        internal class PathVisitor : ExpressionVisitor
+        {
+            internal readonly List<PropertyInfo> Properties = new List<PropertyInfo>();
+
+            protected override Expression VisitMember(MemberExpression node)
+            {
+                this.Properties.Insert(0, node.Member as PropertyInfo);
+                return base.VisitMember(node);
+            }
+        }
+    }
+}
