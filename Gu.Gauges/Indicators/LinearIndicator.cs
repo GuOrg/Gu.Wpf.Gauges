b268d752db193f4fa151c8d30db792677dc825be
Johan Larsson <johan_gavle1@hotmail.com>
2015-03-02 20:45:17 +0100

    Finished default linear style now I think.

--- Gu.Gauges/Indicators/LinearIndicator.cs
+++ Gu.Gauges/Indicators/LinearIndicator.cs
@@ -1,10 +1,15 @@
-﻿namespace Gu.Gauges
+﻿using System;
+using System.Windows.Controls.Primitives;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace Gu.Gauges
 {
     using System.Linq;
     using System.Windows;
     using System.Windows.Controls;
 
-    using Gu.Gauges.Helpers;
+    using Helpers;
 
     public class LinearIndicator : ContentControl
     {
@@ -16,10 +21,42 @@
 
         public static readonly DependencyProperty GaugeProperty = GaugePropertyKey.DependencyProperty;
 
+        private static readonly DependencyProperty PlacementProperty = DependencyProperty.Register(
+            "Placement",
+            typeof(TickBarPlacement),
+            typeof(LinearIndicator),
+            new PropertyMetadata(default(TickBarPlacement), OnPlacementChanged));
+
+        private static readonly DependencyPropertyKey PlacementTransformPropertyKey = DependencyProperty.RegisterReadOnly(
+            "PlacementTransform",
+            typeof(RotateTransform),
+            typeof(LinearIndicator),
+            new PropertyMetadata(default(RotateTransform)));
+
+        public static readonly DependencyProperty PlacementTransformProperty = PlacementTransformPropertyKey.DependencyProperty;
+
+        public LinearIndicator()
+        {
+            this.PlacementTransform = new RotateTransform();
+            var property = NameOf.Property(() => this.Gauge.Axis.Placement);
+            var placementBinding = new Binding(property)
+            {
+                Source = this,
+                Mode = BindingMode.OneWay
+            };
+            BindingOperations.SetBinding(this, PlacementProperty, placementBinding);
+        }
+
         public LinearGauge Gauge
         {
             get { return (LinearGauge)this.GetValue(GaugeProperty); }
             protected set { this.SetValue(GaugePropertyKey, value); }
+        }
+
+        public RotateTransform PlacementTransform
+        {
+            get { return (RotateTransform)this.GetValue(PlacementTransformProperty); }
+            protected set { this.SetValue(PlacementTransformPropertyKey, value); }
         }
 
         protected override void OnVisualParentChanged(DependencyObject oldParent)
@@ -32,9 +69,34 @@
         {
         }
 
+        protected virtual void OnPlacementChanged(TickBarPlacement placement)
+        {
+            switch (placement)
+            {
+                case TickBarPlacement.Left:
+                    this.PlacementTransform.Angle = 90;
+                    break;
+                case TickBarPlacement.Top:
+                    this.PlacementTransform.Angle = 180;
+                    break;
+                case TickBarPlacement.Right:
+                    this.PlacementTransform.Angle = -90;
+                    break;
+                case TickBarPlacement.Bottom:
+                    this.PlacementTransform.Angle = 0;
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
         private static void OnGaugeChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
         {
             ((LinearIndicator)d).OnGaugeChanged((LinearGauge)e.OldValue, (LinearGauge)e.NewValue);
+        }
+
+        private static void OnPlacementChanged(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            ((LinearIndicator)d).OnPlacementChanged((TickBarPlacement)e.NewValue);
         }
     }
 }
