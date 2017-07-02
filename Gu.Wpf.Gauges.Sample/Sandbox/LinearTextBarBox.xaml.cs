0b440b55b3628561b17b2086971f0dbb10be382f
Johan Larsson <johan_gavle1@hotmail.com>
2017-07-02 23:15:58 +0200

    LinearTextBarBox

---
+++ Gu.Wpf.Gauges.Sample/Sandbox/LinearTextBarBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Wpf.Gauges.Sample.Sandbox
+{
+    using System.Windows.Controls;
+
+    public partial class LinearTextBarBox : UserControl
+    {
+        public LinearTextBarBox()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
