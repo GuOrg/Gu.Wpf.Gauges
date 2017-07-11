16bae4cf7e367fa57e67e0fc676ea3e761eacafb
JohanLarsson <johan_gavle1@hotmail.com>
2017-07-11 16:45:08 +0200

    LinearGaugeBox

---
+++ Gu.Wpf.Gauges.Sample/Sandbox/LinearGaugeBox.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Wpf.Gauges.Sample.Sandbox
+{
+    using System.Windows.Controls;
+
+    public partial class LinearGaugeBox : UserControl
+    {
+        public LinearGaugeBox()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
