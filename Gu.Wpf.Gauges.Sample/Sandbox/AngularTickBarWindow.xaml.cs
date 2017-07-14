536feed51d4067d87a15c7ebd1cedbe13943568f
JohanLarsson <johan_gavle1@hotmail.com>
2017-07-14 15:22:04 +0200

    AngularTickBarWindow

---
+++ Gu.Wpf.Gauges.Sample/Sandbox/AngularTickBarWindow.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Gu.Wpf.Gauges.Sample.Sandbox
+{
+    using System.Windows;
+
+    public partial class AngularTickBarWindow : Window
+    {
+        public AngularTickBarWindow()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
