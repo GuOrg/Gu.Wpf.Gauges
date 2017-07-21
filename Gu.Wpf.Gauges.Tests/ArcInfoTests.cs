363320faed10d6b3633dd1d9e5cb4c7b665dbc69
JohanLarsson <johan_gavle1@hotmail.com>
2017-07-21 15:28:11 +0200

    Fix tests.

--- Gu.Wpf.Gauges.Tests/ArcInfoTests.cs
+++ Gu.Wpf.Gauges.Tests/ArcInfoTests.cs
@@ -14,11 +14,11 @@
         [TestCase("0,0", 2, -180, "0, 2")]
         [TestCase("0,0", 2, 270, "-2, 0")]
         [TestCase("0,0", 2, -270, "2, 0")]
-        [TestCase("1,2", 2, 0, "1, 4")]
+        [TestCase("1,2", 2, 0, "1, 0")]
         [TestCase("1,2", 2, 90, "3, 2")]
         [TestCase("1,2", 2, -90, "-1, 2")]
-        [TestCase("1,2", 2, 180, "1, 0")]
-        [TestCase("1,2", 2, -180, "1, 0")]
+        [TestCase("1,2", 2, 180, "1, 4")]
+        [TestCase("1,2", 2, -180, "1, 4")]
         public void GetPoint(string center, double radius, double angle, string expected)
         {
             var arc = new ArcInfo(center.AsPoint(), radius, 0, 0);
@@ -26,19 +26,19 @@
             Assert.AreEqual(expected, actual.ToString("F0"));
         }
 
-        [TestCase("0,0", 2, 1, 0, "0, 3")]
-        [TestCase("0,0", 2, -1, 0, "0, 1")]
+        [TestCase("0,0", 2, 1, 0, "0, -3")]
+        [TestCase("0,0", 2, -1, 0, "0, -1")]
         [TestCase("0,0", 2, 1, 90, "3, 0")]
         [TestCase("0,0", 2, 1, -90, "-3, 0")]
         [TestCase("0,0", 2, -1, 90, "1, 0")]
         [TestCase("0,0", 2, -1, -90, "-1, 0")]
-        [TestCase("0,0", 2, 1, 180, "0, -3")]
-        [TestCase("0,0", 2, -1, -180, "0, -1")]
-        [TestCase("1,2", 2, 1, 0, "1, 5")]
+        [TestCase("0,0", 2, 1, 180, "0, 3")]
+        [TestCase("0,0", 2, -1, -180, "0, 1")]
+        [TestCase("1,2", 2, 1, 0, "1, -1")]
         [TestCase("1,2", 2, 1, 90, "4, 2")]
         [TestCase("1,2", 2, -1, -90, "0, 2")]
-        [TestCase("1,2", 2, 1, 180, "1, -1")]
-        [TestCase("1,2", 2, -1, -180, "1, 1")]
+        [TestCase("1,2", 2, 1, 180, "1, 5")]
+        [TestCase("1,2", 2, -1, -180, "1, 3")]
         public void GetPointWithOffset(string center, double radius, double offset, double angle, string expected)
         {
             var arc = new ArcInfo(center.AsPoint(), radius, 0, 0);
@@ -46,16 +46,16 @@
             Assert.AreEqual(expected, actual.ToString("F0"));
         }
 
-        [TestCase("0,0", 2, 0, "0, 2")]
+        [TestCase("0,0", 2, 0, "0, -2")]
         [TestCase("0,0", 2, 90, "2, 0")]
         [TestCase("0,0", 2, -90, "-2, 0")]
-        [TestCase("0,0", 2, 180, "0, -2")]
-        [TestCase("0,0", 2, -180, "0, -2")]
-        [TestCase("1,2", 2, 0, "1, 4")]
+        [TestCase("0,0", 2, 180, "0, 2")]
+        [TestCase("0,0", 2, -180, "0, 2")]
+        [TestCase("1,2", 2, 0, "1, 0")]
         [TestCase("1,2", 2, 90, "3, 2")]
         [TestCase("1,2", 2, -90, "-1, 2")]
-        [TestCase("1,2", 2, 180, "1, 0")]
-        [TestCase("1,2", 2, -180, "1, 0")]
+        [TestCase("1,2", 2, 180, "1, 4")]
+        [TestCase("1,2", 2, -180, "1, 4")]
         public void StartPoint(string center, double radius, double angle, string expected)
         {
             var arc = new ArcInfo(center.AsPoint(), radius, angle, 0);
@@ -63,8 +63,8 @@
         }
 
         [TestCase(0, "1, 0")]
-        [TestCase(90, "0, -1")]
-        [TestCase(-90, "0, 1")]
+        [TestCase(90, "0, 1")]
+        [TestCase(-90, "0, -1")]
         [TestCase(180, "-1, 0")]
         [TestCase(-180, "-1, 0")]
         public void GetTangent(double angle, string expected)
@@ -73,29 +73,30 @@
             Assert.AreEqual(expected, arc.GetTangent(angle).ToString("F0"));
         }
 
-        [TestCase("0,0", 2, 0, "0, 2")]
+        [TestCase("0,0", 2, 0, "0, -2")]
         [TestCase("0,0", 2, 90, "2, 0")]
         [TestCase("0,0", 2, -90, "-2, 0")]
-        [TestCase("0,0", 2, 180, "0, -2")]
-        [TestCase("0,0", 2, -180, "0, -2")]
-        [TestCase("1,2", 2, 0, "1, 4")]
+        [TestCase("0,0", 2, 180, "0, 2")]
+        [TestCase("0,0", 2, -180, "0, 2")]
+        [TestCase("1,2", 2, 0, "1, 0")]
         [TestCase("1,2", 2, 90, "3, 2")]
         [TestCase("1,2", 2, -90, "-1, 2")]
-        [TestCase("1,2", 2, 180, "1, 0")]
-        [TestCase("1,2", 2, -180, "1, 0")]
+        [TestCase("1,2", 2, 180, "1, 4")]
+        [TestCase("1,2", 2, -180, "1, 4")]
         public void EndPoint(string center, double radius, double angle, string expected)
         {
             var arc = new ArcInfo(center.AsPoint(), radius, 0, angle);
             Assert.AreEqual(expected, arc.EndPoint.ToString("F0"));
         }
 
-        [TestCase("0,0", 2, 0, 90, "0, 2 2, 0")]
-        [TestCase("0,0", 2, -15, 120, "0, 2 2, 0")]
-        [TestCase("0,0", 2, 90, 0, "2, 0 0, -2 -2 0")]
-        [TestCase("0,0", 2, 0, 180, "0, 2 2, 0 0, -2")]
-        [TestCase("0,0", 2, -180, 0, "0, -2 -2, 0 0, 2")]
-        [TestCase("0,0", 2, 180, 0, "0, -2 -2, 0 0, 2")]
-        [TestCase("0,0", 2, 120, 80, "0, -2 -2, 0 0, 2")]
+        [TestCase("0,0", 2, 0, 90, "0, -2 2, 0")]
+        [TestCase("0,0", 2, -15, 120, "0, -2 2, 0")]
+        [TestCase("0,0", 2, -90, 0, "-2, 0 0, -2")]
+        [TestCase("0,0", 2, -120, 30, "-2, 0 0, -2")]
+        [TestCase("0,0", 2, 0, 180, "0, -2 2, 0 0, 2")]
+        [TestCase("0,0", 2, -180, 0, "0, 2 -2, 0 0, -2")]
+        [TestCase("0,0", 2, 180, 270, "0, 2 -2, 0")]
+        [TestCase("0,0", 2, -120, 80, "-2, 0 0, -2")]
         public void QuadrantPoints(string center, double radius, double startAngle, double endAngle, string expected)
         {
             var arc = new ArcInfo(center.AsPoint(), radius, startAngle, endAngle);
