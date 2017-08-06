2a058209a3e9becdc5279e9316f6bfda26037785
JohanLarsson <johan_gavle1@hotmail.com>
2017-08-06 16:47:39 +0200

    More UI-tests.

---
+++ Gu.Wpf.Gauges.UiTests/AngularTextBarWindowTests.cs
@@ -0,0 +1,18 @@
+namespace Gu.Wpf.Gauges.UiTests
+{
+    using NUnit.Framework;
+
+    public sealed class AngularTextBarWindowTests : WindowTests
+    {
+        public AngularTextBarWindowTests()
+            : base("AngularTextBarWindow")
+        {
+        }
+
+        [Test]
+        public void Loads()
+        {
+            Assert.Pass("Just checking that it loads for now");
+        }
+    }
+}
