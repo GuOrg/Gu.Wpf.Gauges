98004643b7e078fa960e47c397432ca3c1f7b177
JohanLarsson <johan_gavle1@hotmail.com>
2017-08-06 14:12:51 +0200

    Refactor: use windows for sandbox.

--- Gu.Wpf.Gauges.Sample/MainWindow.xaml.cs
+++ Gu.Wpf.Gauges.Sample/MainWindow.xaml.cs
@@ -2,15 +2,11 @@
 {
     using System.Windows;
 
-    /// <summary>
-    ///     Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
         public MainWindow()
         {
             this.InitializeComponent();
-            this.DataContext = new Vm();
         }
     }
 }
