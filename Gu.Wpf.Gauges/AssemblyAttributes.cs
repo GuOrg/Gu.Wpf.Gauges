fab09479a323ff35d7dae47c4ecb6be2bf50741e
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-28 13:46:28 +0100

    New csproj

---
+++ Gu.Wpf.Gauges/AssemblyAttributes.cs
@@ -0,0 +1,8 @@
+using System.Runtime.CompilerServices;
+using System.Windows;
+using System.Windows.Markup;
+
+[assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
+[assembly: XmlnsDefinition("https://github.com/JohanLarsson/Gu.Wpf.Gauges", "Gu.Wpf.Gauges")]
+[assembly: XmlnsPrefix("https://github.com/JohanLarsson/Gu.Wpf.Gauges", "gauges")]
+[assembly: InternalsVisibleTo("Gu.Wpf.Gauges.Tests", AllInternalsVisible = true)]
