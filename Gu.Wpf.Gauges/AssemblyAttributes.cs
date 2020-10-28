c3440565970b8eca7da7c40bcd7289accce90cf4
JohanLarsson <johan_gavle1@hotmail.com>
2020-10-28 13:56:15 +0100

    Use ninject.

--- Gu.Wpf.Gauges/AssemblyAttributes.cs
+++ Gu.Wpf.Gauges/AssemblyAttributes.cs
@@ -5,4 +5,4 @@ using System.Windows.Markup;
 [assembly: ThemeInfo(ResourceDictionaryLocation.None, ResourceDictionaryLocation.SourceAssembly)]
 [assembly: XmlnsDefinition("https://github.com/JohanLarsson/Gu.Wpf.Gauges", "Gu.Wpf.Gauges")]
 [assembly: XmlnsPrefix("https://github.com/JohanLarsson/Gu.Wpf.Gauges", "gauges")]
-[assembly: InternalsVisibleTo("Gu.Wpf.Gauges.Tests", AllInternalsVisible = true)]
+[assembly: InternalsVisibleTo("Gu.Wpf.Gauges.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100793e6ca068d06f91bd717e62ddaf01a4b9517c35a0066be6d026f89daecea6582790ee7d9612041f12510d81a91305e29eb6bec3618632183cd2472e177ff94c855f2bfa51a44462e787ba1e7cf70db807224d7eb9f7212db77d2a55d79d7493c21105dbcbc60fe41f61f170bbea6de1dcd2a5016f71626102575c265845fdc6", AllInternalsVisible = true)]
